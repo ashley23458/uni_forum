5254fc70859e143e529166632aadbd37538c030e
ashley23458 <ashleybooth1997@gmail.com>
2019-11-10 22:16:54 +0000

    Implement User factory and add to seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ForumsTableSeeder::class);
+        //$this->call(ForumsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
