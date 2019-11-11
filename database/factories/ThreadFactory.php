0364cfc0f47a0e1dd6de1f4a85c5b5177e299f0f
ashley23458 <ashleybooth1997@gmail.com>
2019-11-11 17:10:04 +0000

    Add Thread factory and seeds

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Thread;
+use Faker\Generator as Faker;
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        'title' => $faker->title,
+        'body' => $faker->paragraph,
+        'likes' => $faker->numberBetween(1, 100),
+        'forum_id' => $faker->numberBetween(1, 6),
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+    ];
+});
