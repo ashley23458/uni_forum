17415791e9cf4d0d197b2035c635e2a9326c043e
ashley23458 <ashleybooth1997@gmail.com>
2020-03-17 21:04:49 +0000

    Add google auth routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 Route::get('/', 'ForumController@index')->name('home');
 Route::get('/forum/{id}', 'ThreadController@index')->name('forum_threads');
-//Laravel. (2019). Resource Controllers [Way of registering a resourceful route to a controller]. (6.x).
-// Retrieved from https://laravel.com/docs/6.x/controllers#resource-controllers
+Route::get('login/google', 'GoogleAuthController@findOrCreateUser');
+Route::get('login/google/callback', 'GoogleAuthController@callback');
+Route::get('login/google/auth', 'GoogleAuthController@auth');
 Route::resource('thread', 'ThreadController')->except(['index']);
 Auth::routes();
 Route::get('/logout', 'Auth\LoginController@logout');
