59dc2d61eb45c83416011bb7b37136de8dc6595d
ashley23458 <ashleybooth1997@gmail.com>
2020-03-18 14:37:08 +0000

    Add pie chart to home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,36 @@
 @extends('layouts.app')
 @section('title', 'Home')
 @section('content')
-    <!--Bootstrap. (2019). Cards [Contains examples for cards.]. (4.3.1).
-    Retrieved from https://getbootstrap.com/docs/4.3/components/card/-->
-    <div class="card">
-        <div class="card-header">
-            <h2>Forums</h2>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Forums</h2>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($forums as $forum)
+                            <tr>
+                                <th scope="row"><a href="{{ route('forum_threads', ['id' => $forum->id]) }}">{{$forum->name}}</a></th>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-            <table class="table">
-                <thead>
-                <tr>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach ($forums as $forum)
-                    <tr>
-                        <th scope="row"><a href="{{ route('forum_threads', ['id' => $forum->id]) }}">{{$forum->name}}</a></th>
-                        <td>
-                            <span class="badge badge-info">Number of Threads
-                                <span class="badge badge-light">
-                                    {{$forum->threads_count}}
-                                </span>
-                                <span class="sr-only">
-                                    are inside the {{$forum->name}} forum
-                                </span>
-                            </span><br>
-                        </td>
-                        <td></td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-body">
+                    <div id="chart-div"></div>
+                    {!! $lava->render('PieChart', 'Forum', 'chart-div') !!}
+                </div>
+            </div>
         </div>
     </div>
-
 @endsection
