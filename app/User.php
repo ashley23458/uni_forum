aab1bb67bf94b0b108022dd45699804af4106f7a
ashley23458 <ashleybooth1997@gmail.com>
2020-03-18 09:17:21 +0000

    Add name to fillable on user model

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
     use Notifiable;
 
     protected $fillable = [
-        'username', 'email', 'password',
+        'name', 'email', 'password',
     ];
 
     protected $dates = [
