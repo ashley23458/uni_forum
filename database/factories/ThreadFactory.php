ce511bdcd0a7b3d5bd221f7d0b56313fe0f96313
vagrant <vagrant@homestead>
2020-04-21 14:50:01 +0000

    Add trix to thread factory

--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -6,9 +6,10 @@ use App\Thread;
 use Faker\Generator as Faker;
 
 $factory->define(Thread::class, function (Faker $faker) {
+    $paragraph = $faker->paragraph. "<p><strong>" .$faker->paragraph. "</strong></p>";
     return [
+        'thread-trixFields' => ['body' => $paragraph],
         'title' => $faker->sentence,
-        'body' => $faker->paragraph,
         'likes' => $faker->numberBetween(1, 100),
         'forum_id' => $faker->numberBetween(1, 6),
         'user_id' => function () {
