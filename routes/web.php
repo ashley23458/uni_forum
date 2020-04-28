4a23ac44f983ac64536a83989bf48938a23eaab5
ashley23458 <ashleybooth1997@gmail.com>
2020-04-28 12:07:27 +0100

    Add restriction for any undefined social login providers

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,11 @@ Route::group(['middleware' => 'language'], function () {
     });
 
     Route::get('/local/{lang}', 'LocalisationController@setLanguage')->name('language');
-    Route::get('login/{provider}', 'SocialiteController@findOrCreateUser');
-    Route::get('login/{provider}/callback', 'SocialiteController@callback');
-    Route::get('login/{provider}/auth', 'SocialiteController@auth')->name('social_login');
+    Route::get('login/{provider}', 'SocialiteController@findOrCreateUser')->where(['provider' => 'google|github']);
+    Route::get('login/{provider}/callback', 'SocialiteController@callback')->where(['provider' => 'google|github']);
+    Route::get('login/{provider}/auth', 'SocialiteController@auth')
+        ->where(['provider' => 'google|github'])
+        ->name('social_login');
     Auth::routes(['verify' => true]);
     Route::get('/logout', 'Auth\LoginController@logout');
 });
